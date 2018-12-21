9a690adf26232033f98e0d5da111828dbc81798c
sampalm <samuelpalmeira@outlook.com>
2018-12-21 11:44:01 -0200

    adds user create lambda action

---
+++ 04-chat-app/go-packages/new_user.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/sampalm/serverless/04-chat-app/go-packages/chatsess"
+)
+
+type Event struct {
+	Username string
+	Password string
+}
+
+type Response struct {
+	Value       int
+	Description string
+}
+
+func handler(ctx context.Context, ev Event) (Response, error) {
+	sess := session.Must(session.NewSession())
+
+	_, err := chatsess.GetDBUser(ev.Username, sess)
+	if err == nil {
+		return Response{Value: 403, Description: "User already exists"}, nil
+	}
+
+	u := chatsess.NewUser(ev.Username, ev.Password)
+	if err := u.Put(sess); err != nil {
+		return Response{Value: 500, Description: "Could not add to database: " + err.Error()}, nil
+	}
+	return Response{Value: 200, Description: "User added to database"}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
