789a8573fcbfb2abcccd285f9dd50444dbf665af
sampalm <samuelpalmeira@outlook.com>
2019-01-03 11:45:30 -0200

    updates new_user api

--- 04-chat-app/go-packages/new_user.go
+++ 04-chat-app/go-packages/new_user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -33,7 +34,21 @@ func handler(ctx context.Context, ev Event) (Response, error) {
 	if err := u.Put(sess); err != nil {
 		return Response{Value: 500, Description: "Could not add to database: " + err.Error()}, nil
 	}
-	return Response{Value: 200, Description: "User added to database"}, nil
+	if err := chatsess.CreateUserFolder(ev.Username, sess); err != nil {
+		return Response{Value: 202, Description: "User created successfully, but something went wrong with your bucket"}, nil
+	}
+	fl, err := chatsess.TextToSpeech(fmt.Sprintf("Welcome %s!\nThis is your bucket, all of your items will be stored here.", ev.Username), sess)
+	if err != nil {
+		return Response{Value: 202, Description: "User created successfully, but something went wrong with Polly"}, nil
+	}
+	obj := chatsess.Object{
+		Key:  fmt.Sprintf("%s/welcome.mp3", ev.Username),
+		Body: fl,
+	}
+	if err := obj.Put(sess); err != nil {
+		return Response{Value: 202, Description: "User created successfully, but wasn't possible to generate Polly audio"}, nil
+	}
+	return Response{Value: 200, Description: "User created successfully"}, nil
 }
 
 func main() {
